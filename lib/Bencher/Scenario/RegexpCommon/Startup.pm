da58fe68c0020bea6074fb07c23e41405ad569d2
perlancar (@pc-office) <perlancar@gmail.com>
2016-02-17 23:01:42 +0700

    Adjust to App::lcpan::Call 0.07

--- lib/Bencher/Scenario/RegexpCommon/Startup.pm
+++ lib/Bencher/Scenario/RegexpCommon/Startup.pm
@@ -7,7 +7,7 @@ use 5.010001;
 use strict;
 use warnings;
 
-our @modules = grep {!/\ARegexp::Common::(Entry|Other|WithActions.*|_.*)\z/} do { require App::lcpan::Call; @{ App::lcpan::Call::call_lcpan_script(argv=>["modules", "--namespace", "Regexp::Common"]) } }; # PRECOMPUTE
+our @modules = grep {!/\ARegexp::Common::(Entry|Other|WithActions.*|_.*)\z/} do { require App::lcpan::Call; @{ App::lcpan::Call::call_lcpan_script(argv=>["modules", "--namespace", "Regexp::Common"])->[2] } }; # PRECOMPUTE
 
 our $scenario = {
     summary => 'Benchmark module startup overhead of Regexp::Common modules',
